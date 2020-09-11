dfc104c86e69d879d78f7cff0c38c83958eb2e36
Viktoriia Petrova <etrovavica1221@gmail.com>
2020-09-11 11:55:42 +0100

    Add search bar-find by title

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,22 +1,45 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
 /* eslint-disable quotes */
-import React from 'react';
+import React, { useState } from 'react';
+import { Link, useLocation, useHistory } from 'react-router-dom';
 import qs from 'qs';
-import { Link, useLocation } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import '../styles/SideBar.css';
 
 const SideBar = () => {
+  const [query, setQuery] = useState("");
+  const history = useHistory();
   const { search } = useLocation();
+
   const buildQueryString = (operation, valueObj) => {
     const currentQueryParams = qs.parse(search, { ignoreQueryPrefix: true });
     const newQueryParams = {
       ...currentQueryParams,
-      [operation]: JSON.stringify(valueObj),
+      [operation]: JSON.stringify({
+        ...JSON.parse(currentQueryParams[operation] || '{}'),
+        ...valueObj,
+      }),
     };
-    return qs.stringify(newQueryParams, { addQueryPrefix: true }, { encode: false });
+    return qs.stringify(newQueryParams, { addQueryPrefix: true, encode: false });
+  };
+  const handleSearch = (e) => {
+    e.preventDefault();
+    const newQueryString = buildQueryString('query', { title: { $regex: query } });
+    history.push(newQueryString);
   };
 
   return (
     <nav className="sidebar">
+      <form onSubmit={handleSearch} id="search-form">
+        <input
+          placeholder="Search"
+          type="text"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button id="search-button" type="submit"><FontAwesomeIcon icon={faSearch} /></button>
+      </form>
       <ul className="sidebar-links">
         <h1>Filter by city:</h1>
         <Link to={buildQueryString('query', { city: 'Manchester' })}>
